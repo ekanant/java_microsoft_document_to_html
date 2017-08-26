77900cb848eab47b0ec25c48c64df9dea10c4af7
Ekanant <ekanant2535@gmail.com>
2017-08-26 08:06:22 +0700

    Complete convert excel to html (.xls, .xlsx)

--- src/Main.java
+++ src/Main.java
@@ -15,12 +15,29 @@ public class Main {
 		writer.println(docHtml);
 		writer.close();
 
-		String docxPath = new File(".").getCanonicalPath() + File.separator + "docx.docx";
+		String docxPath = appPath + File.separator + "docx.docx";
 		String docxHtml = WordDocxConverter.convertToHtml(docxPath);
 		PrintWriter writer2 = new PrintWriter(appPath + File.separator + "docx.html", "UTF-8");
 		writer2.println(docxHtml);
 		writer2.close();
-
+		
+		String xlsPath = appPath + File.separator + "xls.xls";
+		StringBuilder sb = new StringBuilder();
+		ExcelConverter x = ExcelConverter.create(xlsPath, sb);
+		x.setCompleteHTML(true);
+        x.printPage();
+        PrintWriter writer3 = new PrintWriter(appPath + File.separator + "xls.html", "UTF-8");
+		writer3.println(sb.toString());
+		writer3.close();
+		
+		String xlsxPath = appPath + File.separator + "xlsx.xlsx";
+		StringBuilder sb2 = new StringBuilder();
+		ExcelConverter x2 = ExcelConverter.create(xlsxPath, sb2);
+		x2.setCompleteHTML(true);
+        x2.printPage();
+        PrintWriter writer4 = new PrintWriter(appPath + File.separator + "xlsx.html", "UTF-8");
+		writer4.println(sb2.toString());
+		writer4.close();
 	}
 
 }
