9e13b25c8bf679b144cb3b09395af6ce54248a7b
Ekanant <ekanant2535@gmail.com>
2017-08-30 08:45:03 +0700

    Add function to merge multi slide into one photo

--- src/PptxConverter.java
+++ src/PptxConverter.java
@@ -8,6 +8,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.poi.xslf.usermodel.XMLSlideShow;
@@ -46,4 +47,58 @@ public class PptxConverter {
 	      }
 	      ppt.close();
 	}
+	
+	public static void convertToOnePhoto(String inputFile) throws FileNotFoundException, IOException
+	{
+		File file=new File(inputFile);
+	    XMLSlideShow ppt = new XMLSlideShow(new FileInputStream(file));
+		
+		//getting the dimensions and size of the slide 
+		Dimension pgsize = ppt.getPageSize();
+		List<XSLFSlide> slide = ppt.getSlides();
+		int gapBetweenSlide = 50;
+		List<BufferedImage> slideRendered = new ArrayList<BufferedImage>();
+
+		for (int i = 0; i < slide.size(); i++) {
+			
+			BufferedImage img = new BufferedImage(pgsize.width, 
+					(pgsize.height + gapBetweenSlide) * slide.size(),
+					BufferedImage.TYPE_INT_RGB);
+			Graphics2D graphics = img.createGraphics();
+			//clear the drawing area
+			graphics.setPaint(Color.BLACK);
+			graphics.fill(new Rectangle2D.Float(0, pgsize.height, pgsize.width, pgsize.height));
+
+			//render
+			slide.get(i).draw(graphics);
+		     
+			slideRendered.add(img);
+		     
+		}
+		
+		BufferedImage mergeImage = new BufferedImage(pgsize.width, 
+				(pgsize.height + gapBetweenSlide) * slide.size(),
+				BufferedImage.TYPE_INT_RGB);
+		Graphics2D graphics = mergeImage.createGraphics();
+		int offsetToDraw = 0;
+		for(int i = 0 ; i < slideRendered.size() ; i++)
+		{
+			offsetToDraw = (int)(i * pgsize.getHeight());
+			if(i > 0) offsetToDraw += gapBetweenSlide;
+			graphics.drawImage(slideRendered.get(i), null, 0, offsetToDraw);
+		}
+		
+		
+
+		
+		//creating an image file as output
+		FileOutputStream out = new FileOutputStream("pptx_image.png");
+		javax.imageio.ImageIO.write(mergeImage, "png", out);
+		ppt.write(out);
+		out.close();	
+		
+		
+		ppt.close();
+	}
 }
+
